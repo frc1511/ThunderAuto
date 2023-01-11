4ab0bcb5acc9ae59fc967f98c57bcb6861e23551
Peter Lilley <71025767+petelilley@users.noreply.github.com>
2023-01-11 07:30:52 -0500

    Fixed field coordinate problem

--- lib/popups/new_project.cpp
+++ lib/popups/new_project.cpp
@@ -9,7 +9,7 @@
 NewProjectPopup::NewProjectPopup() { }
 
 const Field field_constants[] {
-  Field(Field::BuiltinImage::FIELD_2023, ImVec2(0.9739999771118164, 0.9825640916824341), ImVec2(0.02800000086426735, 0.018844246864318848)),
+  Field(Field::BuiltinImage::FIELD_2023, ImVec2(0.02800000086426735, 0.018844246864318848), ImVec2(0.9739999771118164, 0.9825640916824341)),
   Field(Field::BuiltinImage::FIELD_2022, ImVec2(0.12f, 0.16f), ImVec2(0.88f, 0.84f)),
 };
 
