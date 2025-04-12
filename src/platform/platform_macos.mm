6ef3df0680d7d5b183172aa9f9c41e7b9ca62e12
Peter Lilley <hi.petelilley@gmail.com>
2025-04-12 15:55:43 -0400

    Fixed deprecation warnings for macOS file dialogs

--- src/platform/platform_macos.mm
+++ src/platform/platform_macos.mm
@@ -3,23 +3,34 @@
 #import <AppKit/AppKit.h>
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
 
+NSArray* get_file_types(const FileExtensionList& extensions) {
+      NSMutableArray* types = [[NSMutableArray alloc] init];
+      for (const auto& ext : extensions) {
+        NSString* ext_str = [[NSString alloc] initWithBytes:ext.second
+                                              length:std::strlen(ext.second)
+                                              encoding:NSASCIIStringEncoding];
+
+        // [type_strs addObject:ext_str];
+
+        UTType* t = [UTType typeWithFilenameExtension:ext_str];
+        [types addObject:t];
+      }
+
+      return types;
+}
+
 std::string PlatformMacOS::open_file_dialog(FileType type, const FileExtensionList& extensions) {
+  @autoreleasepool {
+
   NSOpenPanel* open_dialog = [NSOpenPanel openPanel];
   
   if (type == FileType::FILE) {
     [open_dialog setCanChooseFiles:YES];
     [open_dialog setCanChooseDirectories:NO];
     
-
     if (extensions.size()) {
-      NSMutableArray* types = [NSMutableArray array];
-      for (const auto& ext : extensions) {
-        NSString* ext_str = [[NSString alloc] initWithBytes:ext.second
-                                              length:std::strlen(ext.second)
-                                              encoding:NSASCIIStringEncoding];
-        [types addObject:ext_str];
-      }
-      [open_dialog setAllowedFileTypes:types];
+      NSArray* types = get_file_types(extensions);
+      [open_dialog setAllowedContentTypes:types];
     }
   }
   else {
@@ -30,7 +41,9 @@ std::string PlatformMacOS::open_file_dialog(FileType type, const FileExtensionLi
   // Only one file.
   [open_dialog setAllowsMultipleSelection:NO];
   // Show the dialog box.
-  if ([open_dialog runModal] == NSModalResponseOK) {
+  NSModalResponse response = [open_dialog runModal];
+
+  if (response == NSModalResponseOK) {
     NSArray* urls = [open_dialog URLs];
    
     for(NSUInteger i = 0; i < [urls count]; i++) {
@@ -39,21 +52,26 @@ std::string PlatformMacOS::open_file_dialog(FileType type, const FileExtensionLi
       return [path UTF8String];
     }
   }
+  else if (response == NSModalResponseCancel) {
+    // User canceled the dialog.
+    return "";
+  }
+  else {
+    // An error occurred.
+    return "";
+  }
+
   return "";
+
+  }
 }
 
 std::string PlatformMacOS::save_file_dialog(const FileExtensionList& extensions) {
   NSSavePanel* save_dialog = [NSSavePanel savePanel];
 
   if (extensions.size()) {
-    NSMutableArray* types = [NSMutableArray array];
-    for (const auto& ext : extensions) {
-      NSString* ext_str = [[NSString alloc] initWithBytes:ext.second
-                                            length:std::strlen(ext.second)
-                                            encoding:NSASCIIStringEncoding];
-      [types addObject:ext_str];
-    }
-    [save_dialog setAllowedFileTypes:types];
+    NSArray* types = get_file_types(extensions);
+    [save_dialog setAllowedContentTypes:types];
   }
   
   // Show the dialog box.
