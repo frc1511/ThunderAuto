85c4675b489e902c963454acd429fc9beeb1caf4
Peter Lilley <hi.petelilley@gmail.com>
2025-04-16 10:19:16 -0400

    Fixed floating-point equality

--- include/ThunderAuto/math_util.hpp
+++ include/ThunderAuto/math_util.hpp
@@ -3,6 +3,6 @@
 #include <ThunderAuto/thunder_auto.hpp>
 
 inline bool float_eq(float x, float y) {
-  return std::fabs(x - y) < std::numeric_limits<float>::epsilon();
+  return std::fabs(x - y) < 1e-6;
 }
 
